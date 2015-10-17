a16e592a9241587eeae18c686b5d1477834a1c8c
villeguillo <vicenteortegaaguado@gmail.com>
2015-10-17 11:27:59 +0200

    Clase22_tarea3_0.2

--- tarea3/tarea3/ViewController.swift
+++ tarea3/tarea3/ViewController.swift
@@ -10,27 +10,92 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var finish = true
+    @IBOutlet weak var result: UILabel!
     
-    var bola = ["Piedra":1,"Papel":2,"Tijera":3]
+    @IBOutlet weak var you: UIImageView!
     
-    while(finish){
+    @IBOutlet weak var rival: UIImageView!
+    
+    func rivalOpption(){
+        
+        rival.image = UIImage(named: "tijeras.png")
+    }
     
     @IBAction func button1() {
         
+        you.image = UIImage(named: "piedra.jpg")
+        
+        rivalOpption()
+        
+        if you.image == "piedra.jpg" {
+            
+            result.text = "Continue"
+        }
+        
+        if you.image == "papel.jpg" {
+            
+            result.text = "Lose"
+        }
+        
+        if you.image == "tijeras.png" {
+            
+            result.text = "Win"
+        }
         
     }
 
     @IBAction func button2() {
         
+        you.image = UIImage(named: "papel.jpg")
         
+        rivalOpption()
+        
+        if "papel.jpg" == you.image {
+            
+            result.text = "Continue"
+        }
+        
+        if you.image == "tijeras.png" {
+            
+            result.text = "Win"
+        }
+        
+        if you.image == "tijeras.png" {
+            
+            result.text = "Lose"
+        }
     }
 
     @IBAction func button3() {
         
+        you.image = UIImage(named: "tijeras.png")
         
+        rivalOpption()
+        
+        if "tijeras.png" == you.image {
+            
+            result.text = "Continue"
+        }
+        
+        if you.image == "piedra.jpg" {
+            
+            result.text = "Lose"
+        }
+        
+        if you.image == "papel.jpg" {
+            
+            result.text = "Win"
+        }
     }
-    
+
+    override func viewDidLoad() {
+        
+        while(result != "Win" || result != "Lose") {
+            
+            
+        }
     }
 }
+
+
 
